5c4eada46a119656fcde554343b7e747c70d3d9b
nickhendo <50039828+nickhendo@users.noreply.github.com>
2025-09-08 09:36:49 +1000

    Move python packages back into flake.nix

--- flake.nix
+++ flake.nix
@@ -20,15 +20,36 @@
           sqlc
           postgresql
           lldb
+          watchman
         ];
         buildInputs = with pkgs; [];
 
+        jinja2-fragments = python.pkgs.buildPythonPackage rec {
+        pname = "jinja2_fragments";
+        version = "1.9.0"; # Check PyPI for latest version
+        format = "wheel";
+
+        src = python.pkgs.fetchPypi rec {
+          inherit pname version;
+          format = "wheel";
+          dist = python;
+          python = "py3";
+          sha256 = "sha256-abkefi8yXqfjkeNqmrzFctuWfivzr9NfdPy3j8n4xsU=";
+          # build-system = [ setuptools ];
+        };
+
+        propagatedBuildInputs = with python.pkgs; [
+          jinja2
+        ];
+
+        doCheck = false; # Skip tests to avoid potential issues
+        };
+
         python = pkgs.python3;
         pythonEnv = python.withPackages (ps: [
-          ps.psycopg
-          ps.psycopg2-binary
-          ps.psycopg-pool
-          ps.sqlalchemy
+          ps.pywatchman
+          ps.jinja2
+          jinja2-fragments
         ]);
       in {
         devShells.default = pkgs.mkShell {
