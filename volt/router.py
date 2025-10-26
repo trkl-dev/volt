2a92e61fa29ac7b1138018b5c7c0df6136927bab
nickhendo <50039828+nickhendo@users.noreply.github.com>
2025-10-27 07:43:14 +1100

    Fix formatting

--- volt/router.py
+++ volt/router.py
@@ -13,13 +13,15 @@ from urllib.parse import parse_qs
 
 from . import zig_types as zt
 
-zig_logger = logging.getLogger('volt.zig')
-log = logging.getLogger('volt.py')
+zig_logger = logging.getLogger("volt.zig")
+log = logging.getLogger("volt.py")
+
 
 class Header:
     """
     Format Ref: https://developers.cloudflare.com/rules/transform/request-header-modification/reference/header-format/
     """
+
     name: str
     value: str
 
@@ -39,7 +41,9 @@ class Header:
             if char.isalnum():
                 continue
 
-            raise Exception(f"Header name is invalid. Invalid char: {char}. Valid characters are: a-z, A-Z, 0-9 - and _")
+            raise Exception(
+                f"Header name is invalid. Invalid char: {char}. Valid characters are: a-z, A-Z, 0-9 - and _"
+            )
 
     @classmethod
     def validate_value(cls, value: str) -> None:
@@ -50,7 +54,9 @@ class Header:
             if char.isalnum():
                 continue
 
-            raise Exception(rf"Header name is invalid. Invalid char: {char}. Valid characters are: a-z, A-Z, 0-9, _ :;.,\/\"'?!(){{}}[]@<>=-+*#$&`|~^%")
+            raise Exception(
+                rf"Header name is invalid. Invalid char: {char}. Valid characters are: a-z, A-Z, 0-9, _ :;.,\/\"'?!(){{}}[]@<>=-+*#$&`|~^%"
+            )
 
     @override
     def __repr__(self) -> str:
@@ -59,38 +65,39 @@ class Header:
 
 type FormData = dict[str, list[str]]
 
+
 class HttpRequest:
     method: HTTPMethod
     path: str
     body: str
     body_len: int
-    form_data: FormData 
+    form_data: FormData
     headers: list[Header]
     cookies: http_cookies.SimpleCookie
     query_params: dict[str, str]
-    route_params: dict[str, str|int]
+    route_params: dict[str, str | int]
     hx_request: bool
     hx_fragment: str | None
 
     def __init__(
-            self,
-            method: str,
-            path: str,
-            body: str,
-            body_len: int,
-            form_data: FormData,
-            headers: list[Header],
-            cookies: http_cookies.SimpleCookie,
-            query_params: dict[str, str],
-            route_params: dict[str, str|int],
-        ) -> None:
+        self,
+        method: str,
+        path: str,
+        body: str,
+        body_len: int,
+        form_data: FormData,
+        headers: list[Header],
+        cookies: http_cookies.SimpleCookie,
+        query_params: dict[str, str],
+        route_params: dict[str, str | int],
+    ) -> None:
         self.method = HTTPMethod[method]
         self.path = path
         self.body = body
         self.body_len = body_len
         self.form_data = form_data
         self.headers = headers
-        self.cookies = cookies 
+        self.cookies = cookies
         self.query_params = query_params
         self.route_params = route_params
         self.hx_request = False
@@ -105,13 +112,13 @@ class HttpResponse:
     cookies: http_cookies.SimpleCookie | None
 
     def __init__(
-            self,
-            body: str = "",
-            content_type: str = "text/html",
-            status: HTTPStatus= HTTPStatus.OK,
-            headers: list[Header] | None = None,
-            cookies: http_cookies.SimpleCookie | None = None,
-        ) -> None:
+        self,
+        body: str = "",
+        content_type: str = "text/html",
+        status: HTTPStatus = HTTPStatus.OK,
+        headers: list[Header] | None = None,
+        cookies: http_cookies.SimpleCookie | None = None,
+    ) -> None:
         self.body = body
         self.content_type = content_type
         self.status = status
@@ -125,6 +132,7 @@ class Redirect(HttpResponse):
     Redirect the browser to the location at 'route'.
     This is not htmx-aware at this point
     """
+
     def __init__(self, route: str) -> None:
         headers = [
             Header(name="Location", value=route),
@@ -150,6 +158,7 @@ def htmx_parse_request(request: HttpRequest):
 def wrap_middleware(middleware: Middleware, handler: Handler) -> Handler:
     def wrapped(request: HttpRequest):
         return middleware(request, handler)
+
     return wrapped
 
 
@@ -159,7 +168,7 @@ def create_middleware_stack(handler: Handler, *middlewares: Middleware) -> Handl
     return handler
 
 
-middleware_list: list[Middleware]= []
+middleware_list: list[Middleware] = []
 
 
 def middleware(fn: Callable[[HttpRequest, Handler], HttpResponse]):
@@ -170,14 +179,24 @@ def middleware(fn: Callable[[HttpRequest, Handler], HttpResponse]):
 path_list: list[bytes] = []
 
 
-RouteRegister = TypedDict('RouteRegister', {'path': bytes, 'method': bytes, 'handler': Callable[[HttpRequest], HttpResponse]})
+RouteRegister = TypedDict(
+    "RouteRegister",
+    {"path": bytes, "method": bytes, "handler": Callable[[HttpRequest], HttpResponse]},
+)
 routes: list[RouteRegister] = []
 
 
 def route(path: str, method: str = "GET"):
     """Register a route handler on 'path'"""
-    def decorator(handler_fn: Callable[[HttpRequest], HttpResponse]) -> Callable[[zt.HttpRequestPtr, ctypes.c_void_p, ctypes.c_void_p], ctypes.c_int]:
-        def request_handler(request_ptr: zt.HttpRequestPtr, response_ptr: ctypes.c_void_p, context_ptr: ctypes.c_void_p) -> int:
+
+    def decorator(
+        handler_fn: Callable[[HttpRequest], HttpResponse],
+    ) -> Callable[[zt.HttpRequestPtr, ctypes.c_void_p, ctypes.c_void_p], ctypes.c_int]:
+        def request_handler(
+            request_ptr: zt.HttpRequestPtr,
+            response_ptr: ctypes.c_void_p,
+            context_ptr: ctypes.c_void_p,
+        ) -> int:
             try:
                 log.debug("starting handler")
                 req = request_ptr.contents
@@ -187,25 +206,26 @@ def route(path: str, method: str = "GET"):
                 keys_array = (ctypes.c_char_p * num_query_params)()
                 key_lengths_array = (ctypes.c_size_t * num_query_params)()
                 num_keys = zt.lib.query_params_get_keys(
-                    request_ptr,
-                    keys_array,
-                    key_lengths_array,
-                    num_query_params
+                    request_ptr, keys_array, key_lengths_array, num_query_params
                 )
 
                 query_params: dict[str, str] = {}
                 for i in range(num_keys):
                     if keys_array[i]:
-                        key_bytes = ctypes.string_at(keys_array[i], key_lengths_array[i])
-                        key = key_bytes.decode('utf-8')
+                        key_bytes = ctypes.string_at(
+                            keys_array[i], key_lengths_array[i]
+                        )
+                        key = key_bytes.decode("utf-8")
 
                         value_out = ctypes.c_char_p()
-                        size = zt.lib.query_params_get_value(request_ptr, key_bytes, ctypes.byref(value_out))
+                        size = zt.lib.query_params_get_value(
+                            request_ptr, key_bytes, ctypes.byref(value_out)
+                        )
                         # An empty value is actually valid I think. I.e. /search?query= would be size 0, but valid
                         # if size == 0:
                         #     raise Exception(f"Somehow we have a key: {key}, that can not be found")
                         value_bytes = ctypes.string_at(value_out, size)
-                        value = value_bytes.decode('utf-8')
+                        value = value_bytes.decode("utf-8")
 
                         query_params[key] = value
 
@@ -214,16 +234,13 @@ def route(path: str, method: str = "GET"):
                 keys_array = (ctypes.c_char_p * size)()
                 key_lengths_array = (ctypes.c_size_t * size)()
                 num_keys = zt.lib.route_params_get_keys(
-                    request_ptr,
-                    keys_array,
-                    key_lengths_array,
-                    size
+                    request_ptr, keys_array, key_lengths_array, size
                 )
-                
-                route_params: dict[str, str|int] = {}
+
+                route_params: dict[str, str | int] = {}
                 for i in range(num_keys):
                     key_bytes = ctypes.string_at(keys_array[i], key_lengths_array[i])
-                    key = key_bytes.decode('utf-8')
+                    key = key_bytes.decode("utf-8")
 
                     # This was originally using a Union ctype, but I think the memory layout between the ctypes.Union
                     # and the zig union may be different, causing some memory issues. Either that, or my understanding
@@ -232,42 +249,57 @@ def route(path: str, method: str = "GET"):
                     int_out = ctypes.c_int32()
                     tag_out = ctypes.c_int()
 
-                    size = zt.lib.route_params_get_value(request_ptr, key_bytes, ctypes.byref(int_out), ctypes.byref(str_out), ctypes.byref(tag_out))
+                    size = zt.lib.route_params_get_value(
+                        request_ptr,
+                        key_bytes,
+                        ctypes.byref(int_out),
+                        ctypes.byref(str_out),
+                        ctypes.byref(tag_out),
+                    )
                     if tag_out.value == -1:
-                        raise Exception("Somehow we have a key: {key}, that can not be found")
+                        raise Exception(
+                            "Somehow we have a key: {key}, that can not be found"
+                        )
                     elif tag_out.value == 0:
                         route_params[key] = int_out.value
                     elif tag_out.value == 1:
                         if size == 0:
-                            raise Exception("String type route param with invalid size 0")
+                            raise Exception(
+                                "String type route param with invalid size 0"
+                            )
                         if str_out.value is None:
                             raise Exception("String type route param value is None")
 
                         str_bytes = ctypes.string_at(str_out.value, size)
-                        route_params[key] = str_bytes.decode('utf-8')
+                        route_params[key] = str_bytes.decode("utf-8")
 
                 request_headers: list[Header] = []
                 for i in range(req.num_headers):
-                    request_headers.append(Header(
-                        req.headers[i].name.decode('utf-8'),
-                        req.headers[i].value.decode('utf-8'),
-                    ))
+                    request_headers.append(
+                        Header(
+                            req.headers[i].name.decode("utf-8"),
+                            req.headers[i].value.decode("utf-8"),
+                        )
+                    )
 
-                request_body = ctypes.string_at(req.body, req.body_len).decode('utf-8')
+                request_body = ctypes.string_at(req.body, req.body_len).decode("utf-8")
                 log.debug(f"request body: {request_body}")
 
                 request_cookies = http_cookies.SimpleCookie()
                 form_data: dict[str, list[str]] = {}
                 for header in request_headers:
-                    if header.name.lower() == 'cookie':
+                    if header.name.lower() == "cookie":
                         request_cookies.load(header.value)
-                    if header.name.lower() == 'content-type' and header.value == "application/x-www-form-urlencoded":
+                    if (
+                        header.name.lower() == "content-type"
+                        and header.value == "application/x-www-form-urlencoded"
+                    ):
                         form_data = parse_qs(request_body)
                         log.debug(f"parsed form data: {form_data}")
 
                 request_object = HttpRequest(
-                    method=ctypes.string_at(req.method).decode('utf-8'),
-                    path=ctypes.string_at(req.path).decode('utf-8'),
+                    method=ctypes.string_at(req.method).decode("utf-8"),
+                    path=ctypes.string_at(req.path).decode("utf-8"),
                     body=request_body,
                     body_len=req.body_len,
                     form_data=form_data,
@@ -276,10 +308,12 @@ def route(path: str, method: str = "GET"):
                     query_params=query_params,
                     route_params=route_params,
                 )
-                
+
                 htmx_parse_request(request_object)
 
-                handler_with_middleware = create_middleware_stack(handler_fn, *middleware_list)
+                handler_with_middleware = create_middleware_stack(
+                    handler_fn, *middleware_list
+                )
                 handler_response = handler_with_middleware(request_object)
 
                 num_headers = len(handler_response.headers) + 1
@@ -291,27 +325,30 @@ def route(path: str, method: str = "GET"):
                 header_array = (zt.Header * num_headers)()
 
                 # set content-type as the next headers after the custom headers
-                header_array[0].name = "content-type".encode('utf-8')
-                header_array[0].value = handler_response.content_type.encode('utf-8')
-                
+                header_array[0].name = "content-type".encode("utf-8")
+                header_array[0].value = handler_response.content_type.encode("utf-8")
+
                 if len(handler_response.headers) != 0:
                     for i, header in enumerate(handler_response.headers, start=1):
-                        header_array[i].name = header.name.encode('utf-8')
-                        header_array[i].value = header.value.encode('utf-8')
+                        header_array[i].name = header.name.encode("utf-8")
+                        header_array[i].value = header.value.encode("utf-8")
                 else:
                     log.debug("no custom headers in response")
 
                 if handler_response.cookies is not None:
-                    for i, cookie in enumerate(handler_response.cookies.items(), start=len(handler_response.headers) + 1):
-                        header_array[i].name = "Set-Cookie".encode('utf-8')
-                        header_array[i].value = cookie[1].OutputString().encode('utf-8')
+                    for i, cookie in enumerate(
+                        handler_response.cookies.items(),
+                        start=len(handler_response.headers) + 1,
+                    ):
+                        header_array[i].name = "Set-Cookie".encode("utf-8")
+                        header_array[i].value = cookie[1].OutputString().encode("utf-8")
                 else:
                     log.debug("no cookies in response")
 
                 success: Literal[0, 1] = zt.lib.save_response(
                     context_ptr,
-                    handler_response.body.encode('utf-8'),
-                    len(handler_response.body.encode('utf-8')),
+                    handler_response.body.encode("utf-8"),
+                    len(handler_response.body.encode("utf-8")),
                     handler_response.status.value,
                     header_array,
                     num_headers,
@@ -328,16 +365,20 @@ def route(path: str, method: str = "GET"):
             except Exception as e:
                 log.error(f"Exception occurred: {e}")
                 return 0
+
         cb = zt.CALLBACK(request_handler)
-        p = path.encode('utf-8')
+        p = path.encode("utf-8")
         log.info("registering_route: %s", p)
         path_list.append(p)  # Prevent GC of path
-        routes.append({
-            'path': p,
-            'method': method.encode('utf-8'),
-            'handler': cb,
-        })
+        routes.append(
+            {
+                "path": p,
+                "method": method.encode("utf-8"),
+                "handler": cb,
+            }
+        )
         return cb
+
     return decorator
 
 
@@ -350,15 +391,15 @@ def collect_garbage():
 
 @ctypes.PYFUNCTYPE(None, ctypes.POINTER(ctypes.c_char_p), ctypes.c_size_t, ctypes.c_int)
 def log_message(message_ptr: ctypes.c_char_p, message_len: int, level: int):
-    message = ctypes.string_at(message_ptr, message_len).decode('utf-8')
+    message = ctypes.string_at(message_ptr, message_len).decode("utf-8")
     match level:
-        case 0: 
+        case 0:
             zig_logger.debug(message)
-        case 1: 
+        case 1:
             zig_logger.info(message)
-        case 2: 
+        case 2:
             zig_logger.warning(message)
-        case 3: 
+        case 3:
             zig_logger.error(message)
         case _:
             raise Exception(f"Unexpected level for log_message: {level}")
@@ -374,9 +415,16 @@ def _run_server(server_addr: str, server_port: int):
             routes_array[i].path = r["path"]
             routes_array[i].method = r["method"]
             routes_array[i].handler = r["handler"]
-        
+
         log.info("calling zig run_server")
-        zt.lib.run_server(server_addr.encode('utf-8'), server_port, routes_array, len(routes), collect_garbage, log_message)
+        zt.lib.run_server(
+            server_addr.encode("utf-8"),
+            server_port,
+            routes_array,
+            len(routes),
+            collect_garbage,
+            log_message,
+        )
 
     return run
 
